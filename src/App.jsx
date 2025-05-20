d48c7eee59c877b3f5b59293839a5a2a79908369
AldoDante <dane.acuario79@gmail.com>
2025-05-20 18:19:34 -0300

    Agregando el componente en app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import { useState } from 'react'
 import {useProducts} from './hooks/useProducts'
 import './App.css'
-
+import { ProductForm } from './Components/ProductForm'
 function App() {
-
+ const {
+    products,
+    editingProduct,
+    addProduct,
+    updateProduct,
+    
+  } = useProducts();
   return (
     <>
-     
+     <ProductForm
+        addProduct={addProduct}
+        editingProduct={editingProduct}
+        updateProduct={updateProduct}
+        products={products}
+      />
+    
     </>
   )
 }
